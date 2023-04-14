360a5b6cd04ca2e9fb8215776952d6b5ab6ded04
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-04-14 02:00:29 +0200

    feat: register user model to the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
+from django import forms
 from django.contrib import admin
 
+from .models import User
+
 # Register your models here.
+
+class UserAdminForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
+
+admin.site.register(User, UserAdmin)
