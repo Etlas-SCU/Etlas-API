6713d3fe398c6923cb8bad9e53707ab9906a99e4
Memad2001 <mohamed.emad1351@gmail.com>
2023-04-16 16:18:50 +0200

    add views in tours

--- tours/models.py
+++ tours/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.db.models.signals import pre_delete, post_save
+from django.dispatch import receiver
+from django.core.files.storage import default_storage
 # Create your models here.
 
 
@@ -18,5 +20,17 @@ class Section(models.Model):
 class Image(models.Model):
     tour = models.ForeignKey(Tours,on_delete=models.CASCADE)
     image = models.ImageField(upload_to="tours_images/")
+    def delete(self, *args, **kwargs):
+        # Delete the image file from Backblaze B2 bucket
+        default_storage.delete(self.image.name)
+        super().delete(*args, **kwargs)
 
 
+
+@receiver(pre_delete, sender=Tours)
+def delete_image(sender, instance, **kwargs):
+    """
+    Delete the image file from Backblaze B2 bucket when the instance of the model
+    is deleted.
+    """
+    default_storage.delete(instance.image.name)
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Tours,Section,Image
+from .serializers import ImageSerializers,ToursSerializers,SectionSerializers
 # Create your views here.
+
+
+class ToursList(generics.ListAPIView):
+    queryset = Tours.objects.all()
+    serializer_class = ToursSerializers
+    search_fields = ['title']
+
+
+
+
+class Sectionlist(generics.ListAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializers
+
+
+class Imagelist(generics.ListAPIView):
+    queryset = Image
+    serializer_class = ImageSerializers
+    
