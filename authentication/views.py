82064dcde52e34fc5122eecc1c8db294ca7e54ef
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-04-27 13:42:07 +0200

    feat: create the endpoint for registertion

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from users.models import User
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+class RegisterView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        user_data = serializer.data
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
