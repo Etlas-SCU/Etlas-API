1a099d822785a9ff53e7a19747298106505e8241
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-04-27 15:09:27 +0200

    feat: create the url for verfiy mail view

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,11 @@ register = views.RegisterView.as_view({
     'post': 'create',
 })
 
+verify = views.VerifyEmailView.as_view({
+    'get': 'retrieve',
+})
+
 urlpatterns = [
     path('register/', register, name="register"),
-
+    path('email_verify', verify, name="email_verify"),
 ]
