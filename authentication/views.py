9864b21afbbe9ad3d24e8b8685f0d171e2eb51d7
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-04-27 15:08:01 +0200

    feat: finish register view to send a verfication mail to the new created users and create endpoint for verfiying the user

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,14 @@ from rest_framework import viewsets, status
 from users.models import User
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.shortcuts import redirect
+from .tasks import verification_mail
+import uuid
+import environ
+
+env = environ.Env()
 
 # Create your views here.
 
@@ -16,4 +24,31 @@ class RegisterView(viewsets.ModelViewSet):
         serializer.save()
         user_data = serializer.data
 
+        user = User.objects.get(email=user_data['email'])
+        token = user.email_verification_token
+        current_site = get_current_site(request).domain
+        relative_link = reverse('email_verify')
+        absurl = 'http://'+current_site+relative_link+"?token="+str(token)
+        email_body = 'Hi '+ user.full_name + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        verification_mail.delay(data)
+
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+class VerifyEmailView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        token = request.GET.get('token')
+        try:
+            user = User.objects.get(email_verification_token=uuid.UUID(token))
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return redirect(f"{env('WEB_ROOT_URL')}/signin")
+        except (ValueError, User.DoesNotExist):
+            return redirect(f"{env('WEB_ROOT_URL')}/invalid_token")
