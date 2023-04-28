2b4340a2331b7deaca1edd1fc952679e6d326e78
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-04-28 21:31:59 +0300

    feat: create a permission class and add it to user view to allow only the current user to see his data

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsTheCurrentUser
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -8,3 +10,5 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated & IsTheCurrentUser]
+
