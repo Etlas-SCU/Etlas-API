1c11d07d5e3cdc5676a1a985cfbb85d4d18a485d
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-05-04 19:54:51 +0300

    feat: install the cros headers library and add the needed configrations in settings.py

--- api/settings.py
+++ api/settings.py
@@ -55,17 +55,21 @@ INSTALLED_APPS = [
     'django_celery_results',
     'users',
     'rest_framework_simplejwt.token_blacklist',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'api.urls'
 
