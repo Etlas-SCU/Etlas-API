7021c5682d6487ef2bdc48ae48391415342e249b
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-05-08 21:26:16 +0300

    feat: register the otp model to the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib import admin
 
-from .models import User
+from .models import User, OTP
 
 # Register your models here.
 
@@ -14,3 +14,4 @@ class UserAdmin(admin.ModelAdmin):
     form = UserAdminForm
 
 admin.site.register(User, UserAdmin)
+admin.site.register(OTP)
