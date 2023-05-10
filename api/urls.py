72a34c7d5edb40b6cd9866a5939205513f1947fe
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-10 12:33:26 +0300

    Add pre_delete signal to Era model

--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -36,5 +36,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("api/",include("Article.urls")),
+    path("api/", include("Article.urls")),
 ]
--- timeline/models.py
+++ timeline/models.py
@@ -1,11 +1,14 @@
 import math
 
+from django.core.files.storage import default_storage
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 class HistoryTimeline(models.Model):
     """ HistoryTimeline model for timeline app """
-    timeline_name = models.CharField(max_length=200)
+    timeline_name = models.CharField(max_length=200, default='New Timeline')
     timeline_start = models.IntegerField(default=-math.inf)
     timeline_end = models.IntegerField(default=math.inf)
     timeline_description = models.TextField(default="")
@@ -16,12 +19,20 @@ class HistoryTimeline(models.Model):
 
 class Era(models.Model):
     """ Era model for timeline app """
-    era_name = models.CharField(max_length=200)
+    era_name = models.CharField(max_length=200, default='New Era')
     era_start = models.IntegerField(default=-math.inf)
     era_end = models.IntegerField(default=math.inf)
     era_description = models.TextField(default="")
     history_timeline = models.ForeignKey(HistoryTimeline, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='era_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='era_images', blank=True, null=True)
 
     def __str__(self):
         return self.era_name
+
+
+@receiver(pre_delete, sender=Era)
+def delete_era_image(sender, instance, **kwargs):
+    """ Deletes image file from filesystem when corresponding `Era` object is deleted. """
+    if instance.image:
+        path = instance.image.path
+        default_storage.delete(path)
