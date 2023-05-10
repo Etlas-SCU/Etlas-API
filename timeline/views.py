b25868f3c8c4ac84cd0d2a693590976816de40c6
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-10 13:37:11 +0300

    Create HistoryTimelineList, HistoryTimelineDetail views

--- timeline/views.py
+++ timeline/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import HistoryTimeline, Era
+from .serializers import HistoryTimelineSerializer
+from .paginators import CustomLimitOffsetPagination
 
-# Create your views here.
+
+class HistoryTimelineList(generics.ListAPIView):
+    """ List all HistoryTimelines """
+    queryset = HistoryTimeline.objects.all()
+    serializer_class = HistoryTimelineSerializer
+    pagination_class = CustomLimitOffsetPagination
+
+
+class HistoryTimelineDetail(generics.RetrieveAPIView):
+    """ Retrieve a single HistoryTimeline """
+    queryset = HistoryTimeline.objects.all()
+    serializer_class = HistoryTimelineSerializer
