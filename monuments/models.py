b13ceaf227e2b4a622d4816e4e56c6b5e93e2369
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-24 12:06:57 +0300

    Change Monument to TimeStampedModel

--- monuments/models.py
+++ monuments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 
-class Monument(models.Model):
+class Monument(TimeStampedModel):
     name = models.CharField(max_length=100)
