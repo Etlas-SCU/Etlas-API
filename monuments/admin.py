6e6273b7be318ee3de9723d39108f0046de6501d
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-24 12:06:19 +0300

    Create MonumentAdmin, Register Monument to admin panel

--- monuments/admin.py
+++ monuments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Monument
+
+
+class MonumentAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    list_filter = ["created_at"]
+    search_fields = ["name"]
+
+
+admin.site.register(Monument, MonumentAdmin)
