7b7004d9ca149443e69aec60a1d32d20f7eae6e4
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-24 12:09:32 +0300

    Change the name of MonumentSerializers to MonumentSerializer, Refactor views

--- monuments/views.py
+++ monuments/views.py
@@ -1,16 +1,16 @@
 from rest_framework import generics
 
 from monuments.models import Monument
-from monuments.serializers import MonumentSerializers
+from monuments.serializers import MonumentSerializer
 
 
 class MonumentListView(generics.ListAPIView):
     """ List all Monuments """
     queryset = Monument.objects.all()
-    serializer_class = MonumentSerializers
+    serializer_class = MonumentSerializer
 
 
 class MonumentDetailView(generics.RetrieveAPIView):
     """ Retrieve a single Monument """
     queryset = Monument.objects.all()
-    serializer_class = MonumentSerializers
+    serializer_class = MonumentSerializer
