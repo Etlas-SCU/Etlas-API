a68e3296218f2bf7320fd6ee5ab01981c446e58a
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-24 12:52:28 +0300

    Add __str__ method to Monument

--- monuments/models.py
+++ monuments/models.py
@@ -5,3 +5,6 @@ from django_extensions.db.models import TimeStampedModel
 class Monument(TimeStampedModel):
     name = models.CharField(max_length=100)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
