9016eaad80d319b715c58e0b0ea4370d125ae71e
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-25 22:51:33 +0300

    Add favorites/urls

--- favorites/urls.py
+++ favorites/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 app_name = "favorites"
 urlpatterns = [
-
+    path("", views.FavoriteListView.as_view(), name="list"),
+    path("monument/add/", views.FavoriteMonumentCreateView.as_view(), name="monument-add"),
+    path("article/add/", views.FavoriteArticleCreateView.as_view(), name="article-add"),
+    path("monument/delete/", views.FavoriteMonumentDeleteView.as_view(), name="monument-delete"),
+    path("article/delete/", views.FavoriteArticleDeleteView.as_view(), name="article-delete"),
 ]
