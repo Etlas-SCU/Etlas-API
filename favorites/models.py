06cd7dedb9cad03caf0eff424bea541854a537fb
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-25 22:33:18 +0300

    Create Favorite model

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Favorite(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    monument = models.ForeignKey("monuments.Monument", on_delete=models.CASCADE, null=True, related_name="monument")
+    article = models.ForeignKey("articles.Article", on_delete=models.CASCADE, null=True, related_name="article")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["user", "monument", "article"]
+
+    def __str__(self):
+        return f"{self.user} - {self.monument} - {self.article}"
