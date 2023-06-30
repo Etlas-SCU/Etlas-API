10157d870cf58487fa8bd58ee7ff30244a11ea90
aboelsoud <mahmoudabooelsooud@gmail.com>
2023-07-01 02:22:35 +0300

    create the message model to store the contact us messages

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class message(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.subject
+    
