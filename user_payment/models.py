b10292303fd4cdc6ab5694de91682773ea7e7e3f
Ahmed Gamal <>
2023-07-09 19:49:57 +0300

    Create user_payment app

---
+++ user_payment/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from users.models import User
+
+
+class UserPayment(models.Model):
+    app_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_bool = models.BooleanField(default=False)
+    stripe_checkout_id = models.CharField(max_length=500)
+
+
+@receiver(post_save, sender=User)
+def create_user_payment(sender, instance, created, **kwargs):
+    if created:
+        UserPayment.objects.create(app_user=instance)
