f7678f3e9d895e9a0668a0e75d4e9cc1abeaf784
Ahmed Gamal <>
2023-07-11 02:12:41 +0300

    Create a periodic task for refilling the scans left for all users at midnight

--- users/tasks.py
+++ users/tasks.py
@@ -1,13 +1,23 @@
 from celery import shared_task
-from users.models import User
 from django.utils import timezone
+
+from users.models import User
+
 
 @shared_task(bind=True)
 def clear_unverified_users(self):
     users = User.objects.filter(is_verified=False)
-    time = timezone.now() 
+    time = timezone.now()
     for user in users:
         if (time - user.created_at).days >= 30:
             user.delete()
 
     return "Deleted unverified users!"
+
+
+@shared_task(bind=True)
+def refill_scans_left(self):
+    users = User.objects.all()
+    users.update(scans_left=5)
+
+    return "Refilled scans left!"
