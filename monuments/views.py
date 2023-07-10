30023a0ec117eda2ac7e41c28779e1c743ee3175
Ahmed Gamal <>
2023-07-11 01:15:26 +0300

    Edit spacing

--- monuments/views.py
+++ monuments/views.py
@@ -45,7 +45,6 @@ class MonumentDetectionView(APIView):
             return Response({'status': 'You have no scans left'}, status=status.HTTP_403_FORBIDDEN)
         request.user.scans_left -= 1
         request.user.save()
-
         serializer = ImageSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         image_file = request.FILES['image']
