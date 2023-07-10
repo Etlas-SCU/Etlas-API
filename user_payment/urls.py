da346880052f995c5d7bc95006ac982c5710e80c
Ahmed Gamal <>
2023-07-11 01:18:32 +0300

    Add user_payment to api urls

--- user_payment/urls.py
+++ user_payment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'user_payment'
 urlpatterns = [
     path('stripe-webhook/', views.StripeWebhook.as_view(), name='stripe_webhook'),
     path('user-paid/', views.UserPaidView.as_view(), name='user_paid'),
